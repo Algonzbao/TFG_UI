de2ecd19df1dbee151236d85e61b64fdd6c7ec1f
algonzbao <algonz22@ucm.es>
2023-06-29 14:38:57 +0200

    Primeros componentes y Tailwind con error

---
+++ TFG_UI/src/renderer/src/components/Bluetooth.jsx
@@ -0,0 +1,5 @@
+export default function Bluetooth(){
+    return (
+    <h1> Segunda </h1> 
+    )
+}
---
+++ TFG_UI/src/renderer/src/components/WiFi.jsx
@@ -0,0 +1,5 @@
+export default function WiFi(){
+    return (
+    <h1> Primera </h1> 
+    )
+}
