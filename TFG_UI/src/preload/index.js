3bb9ab96028767d140c45ff98ba5be7a1c739199
Ramón Rosa Pérez <ramonros@ucm.es>
2023-07-03 18:07:17 +0200

    mi pana colev

--- TFG_UI/src/preload/index.js
+++ TFG_UI/src/preload/index.js
@@ -1,4 +1,4 @@
-import { contextBridge } from 'electron'
+import { contextBridge, ipcRenderer } from 'electron'
 import { electronAPI } from '@electron-toolkit/preload'
 
 // Custom APIs for renderer
@@ -11,6 +11,12 @@ if (process.contextIsolated) {
   try {
     contextBridge.exposeInMainWorld('electron', electronAPI)
     contextBridge.exposeInMainWorld('api', api)
+    contextBridge.exposeInMainWorld('electronAPI', {
+      cancelBluetoothRequest: (callback) => ipcRenderer.send('cancel-bluetooth-request', callback),
+      bluetoothPairingRequest: (callback) => ipcRenderer.on('bluetooth-pairing-request', callback),
+      bluetoothPairingResponse: (response) =>
+        ipcRenderer.send('bluetooth-pairing-response', response)
+    })
   } catch (error) {
     console.error(error)
   }
--- TFG_UI/src/renderer/src/components/Bluetooth.jsx
+++ TFG_UI/src/renderer/src/components/Bluetooth.jsx
@@ -1,7 +1,22 @@
+import { useEffect } from 'react'
+
 export default function Bluetooth() {
+  async function testIt() {
+    console.log(navigator.bluetooth)
+    const device = await navigator.bluetooth.requestDevice({
+      acceptAllDevices: true
+    })
+    document.getElementById('device-name').innerHTML = device.name || `ID: ${device.id}`
+  }
+
+  useEffect(() => {
+    testIt()
+  }, [])
+
   return (
     <section>
       <h1 className="text-white text-3xl mt-5 px-8"> Bluetooth </h1>
+      <div id="device-name"></div>
     </section>
   )
 }
