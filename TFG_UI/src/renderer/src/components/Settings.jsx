be0307b86b13932ae69f5437bdcecc2132ce2306
RAMON ROSA PEREZ <ramonros@ucm.es>
2023-07-15 18:13:10 +0200

    UI wifi y UI ajustes

--- TFG_UI/src/renderer/src/components/Settings.jsx
+++ TFG_UI/src/renderer/src/components/Settings.jsx
@@ -1,7 +1,35 @@
+import screen from "../assets/screen.png"; 
+import language from "../assets/language.jpeg"; 
+import performance from "../assets/rendimiento.png"; 
+import updates from "../assets/update.png"; 
+
+
 export default function Settings() {
   return (
-    <section>
-      <h1 className="text-white text-3xl mt-5 px-8"> Settings </h1>
+    <section className="px-8">
+      <h1 className="text-white text-3xl mt-5"> Ajustes </h1>
+      <div className="grid grid-cols-2 gap-4 mt-16">
+        <div className=" h-48 rounded-xl bg-contain bg-center bg-no-repeat" style={{ backgroundImage: `url(${screen})` }}>
+          <div className="bg-black bg-opacity-70 h-48 flex rounded-xl">
+            <span className="text-white text-xl px-8 py-4"> Pantalla </span>
+          </div> 
+        </div>
+        <div className=" h-48 rounded-xl bg-contain bg-center bg-no-repeat" style={{ backgroundImage: `url(${language})` }}>
+          <div className="bg-black bg-opacity-70 h-48 flex rounded-xl">
+            <span className="text-white text-xl px-8 py-4"> Idioma </span>
+          </div> 
+        </div>
+        <div className=" h-48 rounded-xl bg-contain bg-center bg-no-repeat" style={{ backgroundImage: `url(${performance})` }}>
+          <div className="bg-black bg-opacity-70 h-48 flex rounded-xl">
+            <span className="text-white text-xl px-8 py-4"> Rendimiento </span>
+          </div> 
+        </div>
+        <div className=" h-48 rounded-xl bg-contain bg-center bg-no-repeat" style={{ backgroundImage: `url(${updates})` }}>
+          <div className="bg-black bg-opacity-70 h-48 flex rounded-xl">
+            <span className="text-white text-xl px-8 py-4"> Actualizaciones </span>
+          </div> 
+        </div>
+      </div>
     </section>
   )
 }
